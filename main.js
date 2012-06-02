e05693297e99b36defc3d51274ecda81bd651a52
Yves Le Pors <y.lepors@novedia-solutions.com>
2012-06-02 10:12:56 +0200

    Li/Lu Jquery tests

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function mainTitle() {
 	//document.getElementById('mainTitle').innerHTML = 'What ?';
 	$('#mainTitle').html('What what ?');
 	$(document.body).css( "background", "lightgrey" );
+	$('li#specificLi').has('ul').css('background-color', 'red');
  }
  
  mainTitle();
