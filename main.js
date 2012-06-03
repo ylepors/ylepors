e999f4f68beea2c39e52feb81502e7c3c2d7c5f4
Yves Le Pors <y.lepors@novediagroup.com>
2012-06-03 12:13:38 +0300

    From Github

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function mainTitle() {
 	//document.getElementById('mainTitle').innerHTML = 'What ?';
 	$('#mainTitle').html('What what ?');
 	$(document.body).css( "background", "lightgrey" );
-	$('li#specificLi').has('ul').css('background-color', 'red');
+	$('li#specificLi').has('ul').css('background-color', 'yellow');
  }
  
  mainTitle();
