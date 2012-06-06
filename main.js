fb9d4d927d09647d91ec24e5d0cfa0c8eb74f8cc
Yves Le Pors <yves@le-pors.net>
2012-06-06 17:37:19 +0200

    Git command line tests

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 function mainTitle() { 
 	//document.getElementById('mainTitle').innerHTML = 'What ?';
-	$('#mainTitle').html('What what ?');
+        $('#mainTitle').html('Yeeeess ?');
 	$(document.body).css( "background", "lightgrey" );
 	$('li#specificLi').has('ul').css('background-color', 'yellow');
  }
